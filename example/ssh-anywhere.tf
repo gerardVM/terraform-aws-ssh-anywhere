99445a9b378dd285a513768f1db1091d03c169e5
gerardVM <101387700+gerardVM@users.noreply.github.com>
2024-03-04 23:05:33 +0100

    first commit

--- example/ssh-anywhere.tf
+++ example/ssh-anywhere.tf
@@ -1,5 +1,5 @@
 module "ssh-anywhere" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ssh-anywhere?ref=0.0.1"
+  source = "github.com/gerardvm/terraform-aws-ssh-anywhere?ref=0.0.1"
 
   instance_id                = "i-1234567890abcdef0"
   instance_security_group_id = "sg-1234567890abcdef0"
