50c5584f591b46aafcae9e1e516a9e3a71b0ffe1
gerardVM <101387700+gerardVM@users.noreply.github.com>
2024-03-05 22:40:18 +0100

    feat/Right VPC implementation (#1)

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 This module creates an EC2 Instance Connect endpoint in an AWS account so you just need to execute the following command to connect via SSH to an EC2 instance:
 
 ```bash
-aws --region <your_aws_region> ec2-instance-connect ssh --instance-id <your_instance_id> --connection-type eice
+aws --region <your_aws_region> ec2-instance-connect ssh \
+--instance-id <your_instance_id> \
+--os-user ec2-user \
+--local-forwarding <local_port>:localhost:<remote_port> \
+--connection-type eice
 ```
--- main.tf
+++ main.tf
@@ -2,14 +2,19 @@ data "aws_instance" "target_instance" {
   instance_id = var.instance_id
 }
 
+data "aws_subnet" "target_instance" {
+  id = data.aws_instance.target_instance.subnet_id
+}
+
 resource "aws_security_group" "allow_outgoing_traffic" {
   name        = "ec2_instance_connect_endpoint_sg"
   description = "Allow TLS outbound traffic"
+  vpc_id      = data.aws_subnet.target_instance.vpc_id
 
   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
     security_groups = [ var.instance_security_group_id ]
   }
 }
@@ -24,6 +29,6 @@ resource "aws_security_group_rule" "allow_eice" {
 }
 
 resource "aws_ec2_instance_connect_endpoint" "eice" {
-  subnet_id          = data.aws_instance.target_instance.subnet_id
+  subnet_id          = data.aws_subnet.target_instance.id
   security_group_ids = [ aws_security_group.allow_outgoing_traffic.id ]
 }
