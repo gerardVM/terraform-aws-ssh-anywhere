b4c10e85d7f4319b0f7e0974de794d4a393c1b62
gerardVM <101387700+gerardVM@users.noreply.github.com>
2024-03-05 21:19:36 +0100

    any vpc implementation

--- main.tf
+++ main.tf
@@ -2,14 +2,19 @@ data "aws_instance" "target_instance" {
   instance_id = var.instance_id
 }
 
+data "aws_subnet" "target_instance" {
+  id = data.aws_instance.target_instance.subnet_id
+}
+
 resource "aws_security_group" "allow_outgoing_traffic" {
   name        = "ec2_instance_connect_endpoint_sg"
   description = "Allow TLS outbound traffic"
+  vpc_id      = data.aws_subnet.target_instance.vpc_id
 
   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
     security_groups = [ var.instance_security_group_id ]
   }
 }
