8ca0dbe8ff760eca66433c4e50a082663e2de0d3
gerardVM <101387700+gerardVM@users.noreply.github.com>
2024-03-06 23:13:15 +0100

    Implementing port variable

--- example/ssh-anywhere.tf
+++ example/ssh-anywhere.tf
@@ -1,7 +1,7 @@
 module "ssh-anywhere" {
   source = "github.com/gerardvm/terraform-aws-ssh-anywhere?ref=0.1.0"
 
-  instance_id                = "i-0cacfb3156dd98072"
-  instance_security_group_id = "sg-0e1568c54d7cbad1b"
+  instance_id                = "i-1234567890abcdef0"
+  instance_security_group_id = "sg-1234567890abcdef0"
   connection_ports           = [ 22, 3389 ] # Optional. Just these two ports are accepted. Default is 22
 }
